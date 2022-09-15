ecc3e72183e7623d658eedf3094c40799743230f
周洋舟 <65344427+newboat@users.noreply.github.com>
2022-09-15 12:06:31 +0800

    Update WeChatAds.js

--- Rewrite/WeChatAds.js
+++ Rewrite/WeChatAds.js
@@ -3,13 +3,13 @@
 ***************************
 QuantumultX:
 [rewrite_local]
-^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/newboat/QuantumultX/main/Rewrite/WeChatAds.js
 [mitm]
 hostname = mp.weixin.qq.com
 ***************************
 Surge4 or Loon:
 [Script]
-http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/newboat/QuantumultX/main/Rewrite/WeChatAds.js
 [MITM]
 hostname = mp.weixin.qq.com
 **************************/
